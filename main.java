33d8c925e0f430a6d5e9665b1c1a87d1d1405113
fvictoria <41639211+fvictoria@users.noreply.github.com>
2019-03-19 21:12:16 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+package com.company;
+/**
+ * This is a class that tests the Card class.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Card one = new Card("Ten", "Spades", 10);
+        Card two = new Card("Ten", "Hearts", 10);
+        Card three = new Card("Ten", "Diamonds", 10);
+
+    }
+}
